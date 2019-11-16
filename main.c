acf39bf13b9a9a3d297e9057697932bbf1341b95
Maisha Miah <mmiah00@stuy.edu>
2019-11-16 17:47:23 -0500

    started

---
+++ main.c
@@ -0,0 +1,31 @@
+/*
+list all the files in te direcotry
+specify which files are direcotires
+show the total size of all the regular files in the directory
+  note that you do not have to recursively go through any subdirectories to find their size for this part (unless you want to)
+have the program scan the current directory
+possible enhancements:
+  recursively list the files in any subdirectories, update the total to include the total size of subdirecotires
+  print out the size in a more readable formate (like using KB, MS, GB for byte prefixes)
+  make it look more like $ls -l
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+void gothroughfiles (DIR * d) {
+  DIR * now = opendir (d);
+  while (now != NULL) {
+    printf (now.d_name); printf ("\n");
+    now = readdir (now);
+  }
+}
+
+int main () {
+  return 0;
+}
+
